91d7ed0e1b3c2fe11eb16902c0e2e96f7f739c9c
isuru <isuru.bandara0306@gmail.com>
2024-05-21 15:39:12 +0530

    Add slider modal

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,6 +1,16 @@
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
-import { Box, Image } from "@chakra-ui/react";
+
+import {
+  Box,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 type SlideImageProps = {
   imageList: {
@@ -10,26 +20,54 @@ type SlideImageProps = {
 };
 
 export default function ImageSlider({ imageList }: Readonly<SlideImageProps>) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const openModalSlider = (imgIndex: number) => {
+    setCurrentImageIndex(imgIndex);
+    setIsModalOpen(true);
+  };
   const renderImages = () => {
-    return imageList.map((imageLink) => {
+    return imageList.map((imageLink, i) => {
       return (
         <Image
+          onClick={() => openModalSlider(i)}
           src={imageLink.url}
           boxShadow="xl"
           borderRadius="xl"
-          key={imageLink.caption}
+          key={imageLink.url}
         />
       );
     });
   };
   const slider = (
-    <Slide infinite={false} arrows={imageList.length > 1} autoplay={false}>
+    <Slide
+      defaultIndex={currentImageIndex}
+      infinite={false}
+      arrows={imageList.length > 1}
+      autoplay={false}
+    >
       {renderImages()}
     </Slide>
+  );
+
+  const modalSlider = (
+    <Modal
+      size="6xl"
+      isOpen={isModalOpen}
+      onClose={() => setIsModalOpen(false)}
+    >
+      <ModalOverlay />
+      <ModalContent bg="transparent" boxShadow={"none"}>
+        <ModalCloseButton zIndex={1} bg="white" />
+        <ModalBody>{slider}</ModalBody>
+      </ModalContent>
+    </Modal>
   );
   return (
     <>
       <Box w={350}>{slider}</Box>
+      {modalSlider}
     </>
   );
 }
